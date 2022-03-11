5f49cfa37fa10ae1fcf5ab2e912c3537b70bd379
Yeabsira Nigussie <nigusseyeabsira@gmail.com>
2022-03-11 20:15:14 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - write to stdout
+ * Return: Always 0
+ */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-  write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n"));
-
-  return (1);
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
